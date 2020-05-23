38bebec1c3fba29327cc379ab1d66f41f5708aa5
luckmerlin <676046361>
2020-05-23 22:27:47 +0800

    Initial

--- Test/src/main/java/com/merlin/test/DownloadTask.java
+++ Test/src/main/java/com/merlin/test/DownloadTask.java
@@ -8,18 +8,28 @@ import com.merlin.task.FileTask;
 import com.merlin.task.FromTo;
 
 import java.io.File;
+import java.io.InputStream;
 
 public abstract class DownloadTask extends FileTask<FromTo<Uri, File>> {
 
+    protected InputStream onOpenStream(){
+        return null;
+    }
+
     @Override
-    protected Controller onExecute(FromTo<Uri, File> arg, Callback... callbacks) {
+    protected final Controller onExecute(FromTo<Uri, File> arg, Callback... callbacks) {
         Uri uri=null!=arg?arg.getFrom():null;
         File to=null!=arg?arg.getTo():null;
         if (null==uri||null==to){
             notifyUpdate(FINISH,"Download task arg is NULL.",this,callbacks);
             return null;
         }
-
+        InputStream is=onOpenStream();
+        if (null==is){
+            notifyUpdate(FINISH,"Download task arg is NULL.",this,callbacks);
+            return null;
+        }
+//        is.read();
         return super.onExecute(arg, callbacks);
     }
 }
